1075a975dd9dccec13eb5ca47580eaf655aa8115
Jakub Tomana <kubakikmodziasz@gmail.com>
2018-08-18 16:08:54 +0200

    Enums
    Signed-off-by: Jakub Tomana <kubakikmodziasz@gmail.com>

---
+++ OgameController/miningBuilding.go
@@ -0,0 +1,12 @@
+package OgameController
+
+const (
+	METAL_MINE            = 1
+	CRYSTAL_MINE          = 2
+	DEUTERIUM_EXTRACTOR   = 3
+	POWER_PLANT           = 4
+	DEUTERIUM_POWER_PLANT = 5
+	METAL_STORAGE         = 7
+	CRYSTAL_STORAGE       = 8
+	DEUTERIUM_STORAGE     = 9
+)
---
+++ OgameController/stationBuilding.go
@@ -0,0 +1,12 @@
+package OgameController
+
+const (
+	ROBOT_FACTORY  = 0
+	SHIP_YARD      = 1
+	LAB            = 2
+	DEPOSIT        = 3
+	ROCKET_SILO    = 4
+	NANITE_FACTORY = 5
+	TERRAFORMER    = 6
+	DOCK           = 7
+)
